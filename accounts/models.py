11d3675713c8b84cd047afeb16356fc493202721
RamonaEJSpence <ramonaejspence@gmail.com>
2020-03-06 11:24:27 -0500

    initial commit

---
+++ accounts/models.py
@@ -0,0 +1,10 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    pass
+
+
+# http://docs.djangoproject.com/en/3.0/topics/auth/customizing/#referencing-the-user-model
