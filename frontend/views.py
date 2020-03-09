567c6e551cfb43a96340d33c4a1f3a834cf904ce
RamonaEJSpence <ramonaejspence@gmail.com>
2020-03-09 09:24:07 -0400

    added some API stuff for testing

--- frontend/views.py
+++ frontend/views.py
@@ -4,12 +4,27 @@ from django.views.generic import View
 from django.http import HttpResponse
 from django.conf import settings
 
+from clarifai.rest import ClarifaiApp
+
+app = ClarifaiApp(api_key = CLARIFAI_API_KEY)
+
+
 
 class IndexView(View):
     """
     Serves the compiled frontend entry point (only works if you have run `npm
     run build`).
     """
+
+    app = ClarifaiApp()
+    model = app.public_models.food_model
+    response = model.predict_by_url('https://www.crowdedkitchen.com/wp-content/uploads/2019/06/Vegan-Pizza-with-cauliflower-crust-11.jpg')
+
+    concepts = response['outputs'][0]['data']['concepts']
+    for concept in concepts:
+        print(concept['name'], concept['value']) ##prints concepts' name and value(probability) in terminal console.
+
+
     def get(self, request, *args, **kwargs):
         try:
             with open(os.path.join(settings.REACT_APP_DIR, 'build', 'index.html')) as f:
