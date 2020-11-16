cf80ebba06ec0312a565552ac33099e19a00e2fc
RamonaEJSpence <ramonaejspence@gmail.com>
2020-11-16 13:36:00 -0500

    added social app in settings, added allauth.urls path

--- conf/urls.py
+++ conf/urls.py
@@ -21,6 +21,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
     path('rest-auth/', include('rest_auth.urls')),
     path('rest-auth/registration/', include('rest_auth.registration.urls')),
     path('', include('frontend.urls', namespace='frontend')),
