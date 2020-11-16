c4562f1ccaf87332ba81f55531e1c5d6d5a35463
RamonaEJSpence <ramonaejspence@gmail.com>
2020-11-16 15:52:34 -0500

    added button for google login, installed django-cors-headers

--- accounts/views.py
+++ accounts/views.py
@@ -4,8 +4,15 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from .serializers import *
 
+from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
+from rest_auth.registration.views import SocialLoginView
 
 class UserListView(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+class GoogleLoginView(SocialLoginView):
+    adapter_class = GoogleOAuth2Adapter
+
+
