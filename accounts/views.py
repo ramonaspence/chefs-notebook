270b9c166df04e8b44312a960dfaf262277fc395
RamonaEJSpence <ramonaejspence@gmail.com>
2020-11-17 16:47:17 -0500

    changed rest-auth to dj_rest_auth registration

--- accounts/views.py
+++ accounts/views.py
@@ -6,7 +6,7 @@ from .serializers import *
 
 from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
 from allauth.socialaccount.providers.oauth2.client import OAuth2Client
-from rest_auth.registration.views import SocialLoginView
+from dj_rest_auth.registration.views import SocialConnectView
 
 
 class UserListView(generics.ListAPIView):
@@ -14,7 +14,7 @@ class UserListView(generics.ListAPIView):
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
 
-class GoogleLoginView(SocialLoginView):
+class GoogleLoginView(SocialConnectView):
     adapter_class = GoogleOAuth2Adapter
     client_class = OAuth2Client
 
