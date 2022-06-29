cbc83c50d79578e8d52e346ecf0ad20194a60574
ramona <ramonaejspence@gmail.com>
2022-06-28 20:19:42 -0400

    reordered views and urls in recipes, added comments

--- recipes/urls.py
+++ recipes/urls.py
@@ -4,12 +4,13 @@ from . import views
 app_name = 'recipes'
 
 urlpatterns = [
-    path('tags/', views.TagListCreateView.as_view(), name='tag_create'),
-    path('<int:pk>/comments/', views.CommentListCreateView.as_view(), name='get_comments'),
-    path('comments/<int:pk>/', views.CommentRUDView.as_view(), name='create_comment'),
     path('', views.RecipeListView.as_view(), name='list_recipe'),
     path('user/<int:pk>/', views.RecipeProfileListView.as_view(), name='profile_recipes'),
+    path('dashboard/', views.RecipeByFollowersListView.as_view(), name='dashboard'),
     path('<int:pk>/', views.RecipeDetailView.as_view(), name='detail_recipe'),
-    path('<int:pk>/versions/', views.RecipeListView.as_view(), name='version_recipe'),
-    path('dashboard/', views.RecipeListByFollowers.as_view(), name='dashboard'),
+    path('tags/', views.TagListCreateView.as_view(), name='tag_create'),
+    path('<int:pk>/comments/', views.CommentListCreateView.as_view(), name='get_comments'),
+    path('comments/<int:pk>/', views.CommentReadEditDeleteView.as_view(), name='create_comment'),
+    
+    
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -22,58 +22,35 @@ stub = service_pb2_grpc.V2Stub(ClarifaiChannel.get_grpc_channel())
 User = get_user_model()
 
 
-class TagListCreateView(generics.ListCreateAPIView):
-    queryset = Tag.objects.all()
-    serializer_class = TagSerializer
-    permission_classes = [IsOwnerOrReadOnly]
-    authentication_classes = [authentication.TokenAuthentication]
-
-class RecipeListByFollowers(generics.ListAPIView):
-    serializer_class = RecipeSerializer
-    permission_classes = [permissions.IsAuthenticated]
-    # authentication_classes = [authentication.TokenAuthentication]
-
-    def get_queryset(self):
-        return Recipe.objects.filter(owner__following__owner=self.request.user).order_by('-date_updated')
-        
-
-
-class RecipeProfileListView(generics.ListAPIView):
-    queryset = Recipe.objects.all()
-    serializer_class = RecipeSerializer
-    permission_classes = [permissions.IsAuthenticated]
-    authentication_classes = [authentication.TokenAuthentication]
-
-    def get_queryset(self, **kwargs):
-        queryset = Recipe.objects.all()
-        user = self.kwargs['pk']
-        if user is not None:
-            queryset = queryset.filter(owner__id=user)
-        return queryset
-
-
 class RecipeListView(generics.ListCreateAPIView):
+    """ View is used to create recipes and list them on the Explore page in 
+    ../frontend/static/src/social-components/Explore.js
+    
+    When a recipe is created and before it is saved in the db, the image gets passed
+    to Clarifai API, which then returns a set of 'tags'. The tags for the image of that
+    recipe are then checked against the db. If the tags already exist, then the recipe gets
+    saved. If the tags don't exist in the db already, they're saved, and then the recipe is
+    saved. 
+    """
     queryset = Recipe.objects.all()
     serializer_class = RecipeSerializer
     filter_backends = (filters.DjangoFilterBackend,)
     filterset_class = RecipeFilter
     permission_classes = [permissions.IsAuthenticated]
-    # authentication_classes = [authentication.TokenAuthentication]
+    authentication_classes = [authentication.TokenAuthentication]
     
     def get_queryset(self):
-        import pdb; pdb.set_trace()
         return super().get_queryset()
     
     
     def save(self, *args, **kwargs):
         recipe = self
-        import pdb; pdb.set_trace()
         metadata = (('authorization', f'Key {settings.CLARIFAI_API_KEY}'),)
 
         if recipe.image != None:
             print('fired')
             request = service_pb2.PostModelOutputsRequest(
-            # This is the model ID of a publicly available General model.
+            # the model ID of a publicly available General model for food
             model_id = 'bd367be194cf45149e75f01d59f77ba7',
             inputs = [
                 resources_pb2.Input(data=resources_pb2.Data(image=resources_pb2.Image(url=recipe.image.url)))
@@ -112,6 +89,31 @@ class RecipeListView(generics.ListCreateAPIView):
         serializer.save(owner = self.request.user)
 
 
+class RecipeProfileListView(generics.ListAPIView):
+    # View is usedd to list recipes on someone's profile. 
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    authentication_classes = [authentication.TokenAuthentication]
+
+    def get_queryset(self, **kwargs):
+        queryset = Recipe.objects.all()
+        user = self.kwargs['pk']
+        if user is not None:
+            queryset = queryset.filter(owner__id=user)
+        return queryset
+
+
+class RecipeByFollowersListView(generics.ListAPIView):
+    # View is used to list a user's followers' recipes on a user's dashboard
+    serializer_class = RecipeSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    # authentication_classes = [authentication.TokenAuthentication]
+
+    def get_queryset(self):
+        return Recipe.objects.filter(owner__following__owner=self.request.user).order_by('-date_updated')
+    
+    
 class RecipeDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Recipe.objects.all()
     serializer_class = RecipeSerializer
@@ -125,6 +127,15 @@ class RecipeUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [IsOwnerOrReadOnly]
     authentication_classes = [authentication.TokenAuthentication]
 
+
+class TagListCreateView(generics.ListCreateAPIView):
+    # used to create and read tags
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+    authentication_classes = [authentication.TokenAuthentication]
+
+
 class CommentListCreateView(generics.ListCreateAPIView):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
@@ -137,10 +148,11 @@ class CommentListCreateView(generics.ListCreateAPIView):
         return Comment.objects.filter(recipe = recipe_id)
 
     def perform_create(self, serializer, **kwargs):
-        recipe = get_object_or_404(Recipe, pk=self.kwargs['pk']) ## grabs recipe instance, so I can assign the object, instead of the id
+        # grabs recipe instance, in order to assign the object, instead of the id
+        recipe = get_object_or_404(Recipe, pk=self.kwargs['pk']) 
         serializer.save(recipe = recipe, owner = self.request.user)
 
-class CommentRUDView(generics.RetrieveUpdateDestroyAPIView):
+class CommentReadEditDeleteView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
     permission_classes = [IsOwnerOrReadOnly]
