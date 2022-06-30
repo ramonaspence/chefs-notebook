a8ce782adbf24297e51ae832282c871a6ef8d59f
ramona <ramonaejspence@gmail.com>
2022-06-29 20:13:24 -0400

    test that unauthenticated request returns 401 unauthorized

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIClient, APITestCase
+from rest_framework.authtoken.models import Token
+from django.urls import reverse
 
-# Create your tests here.
+class TestRecipeListCreate(APITestCase):
+
+    def test_should_not_create_recipe_without_auth(self):
+        # retrieve url
+        url = reverse('api_v1:recipes:list_recipe')
+        # recipe object
+        recipe = {'title': '', 'description': '',
+            'instructions': [], 'ingredients': [],
+            'tags': []}
+        # make a post request
+        response = self.client.post(url, recipe)
+        # test status code is 403
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
