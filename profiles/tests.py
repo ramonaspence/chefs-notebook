fbe7e9a18c6bd94ab21da10b643ed74027bf6802
ramona <ramonaejspence@gmail.com>
2022-07-08 17:20:51 -0400

    test ProfilesListCreateView

--- profiles/tests.py
+++ profiles/tests.py
@@ -18,18 +18,27 @@ class ProfilesAPITestCase(APITestCase):
         token = Token.objects.get(user__username="username")
         self.client.credentials(HTTP_AUTHORIZATION=f"Token {token}")
         
+    def create_profile(self):
+        profile = {
+            'display_name': "display_name",
+            'avatar': File(open('profiles/tmp/cute-avatar.jpeg', 'rb')),
+            'bio': "bio here"}
+        response = self.client.post(reverse('api_v1:profiles:profile'), 
+                                    profile)
+        
+        return response
         
 class TestProfileListCreate(ProfilesAPITestCase):
     
     def test_create_profile_with_auth(self):
         self.authenticate()
-        profile = {
-            'display_name': "display_name",
-            'avatar': File(open('profiles/tmp/ahsoka.jpeg', 'rb')),
-            'bio': "bio here"}
-        response = self.client.post(reverse('api_v1:profiles:profile'), 
-                                    profile)
+        response = self.create_profile()
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        
+        res = self.client.get(reverse('api_v1:profiles:profile'))
+        self.assertEqual(res.data[0]['display_name'], "display_name")
+        self.assertEqual(res.data[0]['bio'], "bio here")
+        
         profile = Profile.objects.get(id = response.data['id'])
         self.assertEqual(profile.display_name, "display_name")
         self.assertEqual(profile.bio, "bio here")
