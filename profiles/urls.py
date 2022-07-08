3d4f6eb09ca484c1eb4e10fc36d017bc4f3b2692
ramona <ramonaejspence@gmail.com>
2022-07-07 21:08:14 -0400

    rename profileUpdateView in urlconf

--- profiles/urls.py
+++ profiles/urls.py
@@ -6,11 +6,13 @@ app_name = "profiles"
 
 urlpatterns = [
     
-    path('', views.ProfileListCreateView.as_view(), name="profile"),  ## this endpoint contains all the profiles, so I can list them out in a 'userlist'
-    path('<int:pk>/', views.ProfileDetailView.as_view(), name='profile_detail'), ## this endpoint contains one user's profile, to be seen by someone else
+    path('', views.ProfileListCreateView.as_view(), name="profile"),
+    ## this endpoint contains one user's profile, to be seen by someone else
+    path('<int:pk>/', views.ProfileDetailView.as_view(), name='profile_detail'), 
+    ## this endpoint contains the logged in user's profile
+    path('user/', views.ProfileRetrieveUpdateView.as_view(), name='user_profile'),
     path('followers/', views.FollowerListView.as_view(), name='list_followers'),
     path('following/', views.FollowingListView.as_view(), name='list_following'),
-    path('user/', views.ProfileUpdateView.as_view(), name='user_profile'), ## this endpoint contains the logged in user's profile
     path('connections/', views.ConnectionListCreateAPIView.as_view(), name='connections'),
     path('connections/<int:pk>/', views.ConnectionRetrieveDestroyView.as_view(), name='remove_connections'),
 
