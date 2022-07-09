86f28bae7fc3201689e9621c6c1c17c5e49b1c19
ramona <ramonaejspence@gmail.com>
2022-07-09 19:50:59 -0400

    add check for this.state.avatar.type that its an image

--- frontend/static/src/user-profile-components/ProfileUpdate.js
+++ frontend/static/src/user-profile-components/ProfileUpdate.js
@@ -3,7 +3,6 @@ import '../App.css';
 import { Redirect } from 'react-router-dom';
 import moment from 'moment';
 import Nav from '../containers/Nav.js';
-import ProfileForm from './profileForm.js';
 
 import axios from 'axios';
 
@@ -25,7 +24,7 @@ class ProfileUpdate extends Component {
 
     this.componentDidMount = this.componentDidMount.bind(this)
     this.handleChange = this.handleChange.bind(this)
-    this.handleImage = this.handleImage.bind(this)
+    this.onImageChange = this.onImageChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
 
   }
@@ -37,11 +36,10 @@ class ProfileUpdate extends Component {
 
   }
 
-  handleImage(e) {
+  onImageChange(e) {
     e.preventDefault();
     let file = e.target.files[0]
     this.setState({[e.target.name]: file});
-
     let reader = new FileReader();
     reader.readAsDataURL(file);
     reader.onloadend = () => this.setState({preview: reader.result});
@@ -49,16 +47,14 @@ class ProfileUpdate extends Component {
 
   handleSubmit(e) {
     e.preventDefault();
-
     let formData = new FormData()
     formData.append('display_name', this.state.display_name)
     formData.append('bio', this.state.bio)
 
 
-    if (this.state.image === File) {
+    if (this.state.avatar.type.startsWith('image/')) {
       formData.append('avatar', this.state.avatar)
-    } else
-
+    }
     axios.patch(`${BASE_URL}/api/v1/profiles/user/`, formData, {
       headers: {
         'Content-Type': 'multipart/form-data',
@@ -75,13 +71,11 @@ class ProfileUpdate extends Component {
       headers: {'Authorization': `Token ${JSON.parse(localStorage.getItem('current-user')).key}`}
     })
     .then(response => this.setState(response.data))
-    .then(res => console.log(this.state))
     .catch(err => console.log(err))
   }
 
 
   render() {
-    console.log('state', this.state)
     if (this.state.redirect) {
       return (<Redirect to="/profile/" />)
     }
@@ -92,7 +86,40 @@ class ProfileUpdate extends Component {
           <div className='row'>
             <div className='col-md-6 col-12 card profile-update-body'>
               <div className='card-body row'>
-                <ProfileForm profile={this.state} preview={this.state.preview}/>
+
+              <form method="post" onSubmit={this.handleSubmit}>
+                
+                <div className="create-display-name col-md-12 col-7 mr-auto">
+                    <label htmlFor="display_name">
+                        Display Name
+                    </label>
+                    <input type='text' name="display_name" defaultValue={this.state.display_name ? this.state.display_name : ''} onChange={this.handleChange} />
+                </div>
+
+                <div className="create-avatar col-md-12 col-5">
+                    { this.state.preview
+                    ?
+                    <div className="create-avatar-preview">
+                        <img src={this.state.preview} alt="don't know about that" />
+                    </div>
+                    :
+                    <div className="create-avatar-preview">
+                    </div>
+                    }
+                    <label htmlFor="avatar">
+                        Upload an Avatar
+                    </label>
+                    <input type="file" name="avatar" onChange={this.onImageChange} />
+                </div>
+
+                <div className="create-bio col-12 mr-auto">
+                    <label htmlFor="bio">
+                        Add Bio:
+                    </label>
+                    <textarea type='text' name="bio" defaultValue={this.state.bio ? this.state.bio : ''} onChange={this.handleChange} />
+                </div>
+                <button>Save Profile</button>
+        </form>
               </div>
 
               <div className='card-footer profile-bio-footer'>
