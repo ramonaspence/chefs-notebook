c1c535befb010394ab4c1328ea9ef7b0c8027633
ramona <ramonaejspence@gmail.com>
2022-07-10 18:21:13 -0400

    add PK to URLConf for FollowerListView and FollowingListView

--- profiles/urls.py
+++ profiles/urls.py
@@ -10,8 +10,8 @@ urlpatterns = [
     path('<int:pk>/', views.ProfileDetailView.as_view(), name='profile_detail'), 
     ## this endpoint contains the logged in user's profile
     path('user/', views.ProfileRetrieveUpdateView.as_view(), name='user_profile'),
-    path('followers/', views.FollowerListView.as_view(), name='list_followers'),
-    path('following/', views.FollowingListView.as_view(), name='list_following'),
+    path('followers/<int:pk>/', views.FollowerListView.as_view(), name='list_followers'),
+    path('following/<int:pk>/', views.FollowingListView.as_view(), name='list_following'),
     path('connections/', views.ConnectionListCreateAPIView.as_view(), name='connections'),
     path('connections/<int:pk>/', views.ConnectionRetrieveDestroyView.as_view(), name='remove_connections'),
 ]
