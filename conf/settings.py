8b73a1a74e0850cc069803fb232f17dfb5a01ce0
ramona <ramonaejspence@gmail.com>
2022-08-22 14:21:14 -0400

    update AWS access key id and secret access key

--- conf/settings.py
+++ conf/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '=$5#xw8&-m8v(!og10s#5u-umul77($aj2g$l0vs^a90o$hkuk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
