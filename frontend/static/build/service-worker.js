85c6e7cbd453da3bd3ace5c302d16aa015c0d776
ramona <ramonaejspence@gmail.com>
2022-08-22 14:07:35 -0400

    setup for deployment on heroku

--- frontend/static/build/service-worker.js
+++ frontend/static/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.db4682573cdbaee9aaea224d1322fbc8.js"
+  "/precache-manifest.a90d061c1e592ed046064630185123c2.js"
 );
 
 self.addEventListener('message', (event) => {
